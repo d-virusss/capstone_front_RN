1e2afb6d3bfe90921e7f04d1b5b528de172223ea
d-virusss <jyg0599@gmail.com>
2020-10-10 21:20:58 +0900

    chat ui 구현 및 refactoring 완료

--- views/chat/chat_index.js
+++ views/chat/chat_index.js
@@ -15,31 +15,9 @@ class ListAvatarExample extends Component {
         <Header />
         <Content>
           <List>
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: "https://picsum.photos/id/1001/200/200" }} />
-              </Left>
-              <Body>
-                <Text onPress={this.callimage}>Kumar Pratik</Text>
-                <Text note>Doing what you like will always keep you happy . .</Text>
-              </Body>
-              <Right>
-                <Text note>3:43 pm</Text>
-              </Right>
-            </ListItem>
-            <ListItem avatar>
-              <Left>
-                <Thumbnail source={{ uri: "https://picsum.photos/id/1001/200/200" }} />
-              </Left>
-              <Body>
-                <Text onPress={this.callimage}>Kumar Pratik</Text>
-                <Text note>Doing what you like will always keep you happy . .</Text>
-              </Body>
-              <Right>
-                <Text note>3:43 pm</Text>
-              </Right>
-            </ListItem>
-            <ListProfile></ListProfile>
+            <ListProfile imageURI="https://picsum.photos/id/3/150/150" title="시스템프로그래밍" body="과제 너무 어렵지만 즐겁다 ㅎㅎ" time="9:02 pm"></ListProfile>
+            <ListProfile imageURI="https://picsum.photos/id/100/150/150" title="캡디" body="아 리액트 ㅡㅡ" time="8:55 pm"></ListProfile>
+            <ListProfile imageURI="https://picsum.photos/id/1001/150/150" title="엄마" body="밥 먹었어?" time="8:49 pm"></ListProfile>
           </List>
         </Content>
       </Container>
@@ -47,26 +25,19 @@ class ListAvatarExample extends Component {
   }
 }
 
-class ListProfile extends Component {
-  state = {
-    imageURI : "",
-    title : "",
-    body : "",
-    time : ""
-  }
-  
+class ListProfile extends Component {  
   render(){
     return(
       <ListItem avatar>
         <Left>
-          <Thumbnail source={this.state.imageURI} />
+          <Thumbnail source={{ uri: this.props.imageURI}} style={{ marginTop: -14 }} />
         </Left>
-        <Body>
-          <Text> {this.state.title} </Text>
-          <Text note> {this.state.body} </Text>
+        <Body style={{paddingVertical: 30}} >
+          <Text> {this.props.title} </Text>
+          <Text note> {this.props.body} </Text>
         </Body>
         <Right>
-          <Text note> {this.state.time} </Text>
+          <Text note> {this.props.time} </Text>
         </Right>
       </ListItem>
     );
