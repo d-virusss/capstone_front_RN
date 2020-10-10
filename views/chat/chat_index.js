5e6c1bca6d4162aba8bbdbf49277cbb363107411
d-virusss <jyg0599@gmail.com>
2020-10-10 17:44:08 +0900

    chat list 구현중

--- views/chat/chat_index.js
+++ views/chat/chat_index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import { 
   Container, Header, Content, List, ListItem, 
-  Left, Body, Right, Thumbnail, Text } from 'native-base';
+  Left, Body, Right, Thumbnail, Text, View } from 'native-base';
 
-import avatar from '../../assets/p1.jpg'
-
-export default class ListAvatarExample extends Component {
+class ListAvatarExample extends Component {
 
   callimage = () => {
     console.log(avatar)
@@ -19,19 +17,60 @@ export default class ListAvatarExample extends Component {
           <List>
             <ListItem avatar>
               <Left>
-                <Thumbnail source={{ uri: "assets/p1.jpg" }} />
+                <Thumbnail source={{ uri: "https://picsum.photos/id/1001/200/200" }} />
               </Left>
               <Body>
-                <Text onPress = {this.callimage}>Kumar Pratik</Text>
+                <Text onPress={this.callimage}>Kumar Pratik</Text>
                 <Text note>Doing what you like will always keep you happy . .</Text>
               </Body>
               <Right>
                 <Text note>3:43 pm</Text>
               </Right>
             </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={{ uri: "https://picsum.photos/id/1001/200/200" }} />
+              </Left>
+              <Body>
+                <Text onPress={this.callimage}>Kumar Pratik</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
+              </Body>
+              <Right>
+                <Text note>3:43 pm</Text>
+              </Right>
+            </ListItem>
+            <ListProfile></ListProfile>
           </List>
         </Content>
       </Container>
     );
   }
 }
+
+class ListProfile extends Component {
+  state = {
+    imageURI : "",
+    title : "",
+    body : "",
+    time : ""
+  }
+  
+  render(){
+    return(
+      <ListItem avatar>
+        <Left>
+          <Thumbnail source={this.state.imageURI} />
+        </Left>
+        <Body>
+          <Text> {this.state.title} </Text>
+          <Text note> {this.state.body} </Text>
+        </Body>
+        <Right>
+          <Text note> {this.state.time} </Text>
+        </Right>
+      </ListItem>
+    );
+  }
+}
+
+export default ListAvatarExample;
