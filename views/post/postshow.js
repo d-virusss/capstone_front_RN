794efae487c4392716f6b073fd55f1bafc97f08c
kenny <kenny@kennyui-MacBookPro.local>
2020-10-22 17:30:49 +0900

    added post show

--- views/post/postshow.js
+++ views/post/postshow.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {Text} from 'native-base';
+import {View, ScrollView} from 'react-native';
+import {Text, Form, Icon, Textarea, Item, Input, Button} from 'native-base';
+
+import CategoryPicker from './categorypicker';
 class PostShow extends Component{
+    constructor(props){
+        super(props);
+        title = '화이트채플';
+        category = '보드게임';
+        price = 10000;
+        bodytext = '어쩌구저쩌구';
+    }
     render(){
-        return(
-            <View style={{flex : 1}}>
-                <Text>success</Text>
+      return(
+        <ScrollView>
+          <View style={{width : '95%', height : '40%', justifyContent : 'center', alignItems: 'center', alignSelf: 'center'}}>
+            <Text>이미지</Text>
+          </View>
+          <View style = {{ alignItems : 'center'}}>
+            <View style = {{ width : '95%',}}>
+              <Form>
+                <Item regular style = {{marginBottom : '3%'}}>
+                    <Text style = {{fontSize : 17, margin : '4%'}}>{title}</Text>
+                </Item>
+                <Item regular style = {{marginBottom : '3%'}}>
+                    <Text style = {{fontSize : 17, margin : '4%'}}>{category}</Text>
+                </Item>
+                <Item regular style = {{marginBottom : '5%', marginTop  :'3%'}}>
+                    <Text style = {{fontSize : 17, margin : '4%'}}>{price + '원'}</Text>
+                </Item>
+                <Item regular style = {{marginBottom : '5%', marginTop  :'3%'}}>
+                    <Text style = {{fontSize : 17, margin : '4%'}}>{bodytext}</Text>
+                </Item>
+                <Button style = {{alignSelf : 'center', marginTop : '3%'}}>
+                  <Icon name = 'person'></Icon>
+                  <Text>거래요청</Text>
+                </Button>
+              </Form>
             </View>
-        );
+          </View>
+        </ScrollView>
+      );
     }
 }
 
