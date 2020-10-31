885f9dc7ba7c88e0f4471e204eb74ef6a26090ba
miny-kim <my6814@gmail.com>
2020-10-31 20:56:54 +0900

    show map

--- views/mypage/location.js
+++ views/mypage/location.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import {StyleSheet, Dimensions, View} from 'react-native';
 
-class MapScreen extends Component {}
-
+class MypageScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.283056,
+            longitude: 127.04366,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.01,
+          }}>
+          <Marker
+            coordinate={{latitude: 37.283056, longitude: 127.04366}}
+            title="this is a marker"
+            description="this is a marker example"
+          />
+        </MapView>
+      </View>
+    );
+  }
+}
+let {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-  btn: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'transparent',
+  container: {
+    height: height,
+    width: width,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
-
-export default MapScreen;
+export default MypageScreen;
