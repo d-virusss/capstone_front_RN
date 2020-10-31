e7481dd787e6a49fd49da5dbc8df53937139100c
kenny <kenny@kennyui-MacBookPro.local>
2020-10-31 18:23:11 +0900

    finished chat without local db

--- views/chat/chat_index.js
+++ views/chat/chat_index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { 
   Container, Header, Content, List, ListItem, 
   Left, Body, Right, Thumbnail, Text,
@@ -9,21 +9,19 @@ import BottomTab from '../shared/bottom_tab';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const api = axios.create({baseURL: 'http://3.35.9.144'});
-var token;
-var chat_id = [];
-chat_id[0] = 1;
+let token;
 
 class ListProfile extends Component {  
   render(){
     return(
       <ListItem avatar>
         <Left>
-          <TouchableOpacity onPress = {() => this.props.navigation.navigate('ChatRoom'), {chat_id : chat_id[0]}}>
+          <TouchableOpacity onPress = {() => this.props.navigation.navigate('ChatRoom'), {chat_id : this.props.chatID}}>
             <Thumbnail source={{ uri: this.props.imageURI}} style={{ marginTop: -14 }} />
           </TouchableOpacity>
         </Left>
         <Body style={{paddingVertical: 30}} >
-          <TouchableOpacity onPress = {() => this.props.navigation.navigate('ChatRoom', {chat_id : chat_id[0]})}>
+          <TouchableOpacity onPress = {() => this.props.navigation.navigate('ChatRoom', {chat_id : this.props.chatID})}>
             <Text> {this.props.title} </Text>
             <Text note> {this.props.body} </Text>
           </TouchableOpacity>
@@ -36,6 +34,8 @@ class ListProfile extends Component {
   }
 }
 function ChatList ({navigation}){
+  
+  const [chats, setChats] = useState([]);
 
   getToken = async () => {
     try{
@@ -49,12 +49,16 @@ function ChatList ({navigation}){
   
   chatGetRequest = () => {
     api
-      .get(`/chats`, null,{ headers : {
-        'Authorization': token
-      }})
+      .get(`/chats`, 
+      { 
+        headers : {
+          'Authorization': token
+        }
+      })
       .then((response) => {
         console.log('success');
         console.log(response);
+        setChats(response.data, [])
       })
       .catch((err) => console.log("err : ", err))
   }
@@ -62,15 +66,26 @@ function ChatList ({navigation}){
   callimage = () => {
     console.log(avatar)
   }
-  getToken();
-  chatGetRequest();
+
+  const makeIndexList = () => {
+    console.log(JSON.stringify(chats));
+    return chats.map((chat) => {
+      return(
+        <ListProfile navigation = {navigation} imageURI="https://picsum.photos/id/3/150/150" title = {chat.chat_info.post_id} body = {chat.chat_info.message} time = "" chatID = {chat.chat_info.id}/> 
+      )
+    })
+  }
+
+  useEffect(() => {
+    getToken(), 
+    setTimeout(chatGetRequest,1000), 
+    console.log("--------------------")
+  })
   return(
     <Container>
         <Content>
           <List>
-            <ListProfile navigation = {navigation} imageURI="https://picsum.photos/id/3/150/150" title="시스템프로그래밍" body="과제 너무 어렵지만 즐겁다 ㅎㅎ" time="9:02 pm"></ListProfile>
-            <ListProfile navigation = {navigation} imageURI="https://picsum.photos/id/100/150/150" title="캡디" body="아 리액트 ㅡㅡ" time="8:55 pm"></ListProfile>
-            <ListProfile navigation = {navigation} imageURI="https://picsum.photos/id/1001/150/150" title="엄마" body="밥 먹었어?" time="8:49 pm"></ListProfile>
+            {makeIndexList()}
           </List>
         </Content>
         <BottomTab navigation = {navigation}></BottomTab>
