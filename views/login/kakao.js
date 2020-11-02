ef9f7d23812c7976ef033060d16d546ce5a1b991
oohyun15 <sakiss@ajou.ac.kr>
2020-11-02 17:36:43 +0900

    Update kakao.js
    - postMessage 받도록 수정

--- views/login/kakao.js
+++ views/login/kakao.js
@@ -2,30 +2,42 @@ import React, {Component} from 'react';
 import {Alert} from 'react-native';
 import {WebView} from 'react-native-webview';
 
-const html = `
-<script>
-function send(){
-  window.ReactNativeWebView.postMessage('hello');
+// const html = `
+// <script>
+// function send(){
+//   window.ReactNativeWebView.postMessage('hello');
 
-}
-</script>
-<button onclick ="send()"> Send</button>
-`;
+// }
+// </script>
+// <button onclick ="send()"> Send</button>
+// `;
 
 class KakaoLoginScreen extends Component {
   onWebViewMessage = (e) => {
+    console.log("kakao")
     Alert.alert(e.nativeEvent.data);
   };
   render() {
     return (
       <WebView
         ref={(webview) => (this.webview = webview)}
+        onMessage={this._onMessage}
         source={{uri: 'http://3.35.9.144/users/auth/kakao'}}
-        //source={{html}}
-        onMessage={this.onWebViewMessage}
+        // source={{html}}
+        // onMessage={this.onWebViewMessage}
         javaScriptEnabled={true}
+        onLoadENd={() => {
+          console.log("LOAD END");
+        }}
+        onError={err => {
+          console.log('ERROR ', err);
+        }}
       />
     );
+  }
+
+  _onMessage = (data) => {
+    alert(data.nativeEvent.data);
   }
 }
 
