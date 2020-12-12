5cec372ba0976ab5ce48fa94fe22a5dc47c2e8d7
kenny <kenny@kennyui-MacBookPro.local>
2020-12-12 17:42:22 +0900

    review image fix

--- views/mypage/write_review.js
+++ views/mypage/write_review.js
@@ -79,7 +79,7 @@ class WriteReviewScreen extends Component {
 
   changeImage = (data) => {
     this.setState({images: data})
-
+	multi_images=[]
     _.each(this.state.images, (image, index) => {
       multi_images.push(new Object)
       multi_images[index].uri = image.sourceURL
